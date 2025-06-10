4a6b0ecc00e0bd215e553b92212b6e34f1ca243f
Satendra Pratap Singh <rajasatyamsingh12345@gmail.com>
2025-06-10 17:06:22 +0530

    Update Header.jsx

--- React_Library/src/components/UI/Header.jsx
+++ React_Library/src/components/UI/Header.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import './header.css'
 import { NavLink } from "react-router-dom"
+import { GiHamburgerMenu } from "react-icons/gi";
+
+
 
 export const Headers = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const handleToggleButton = () => {
+        setShowMenu(!showMenu);
+    }
     return <header>
         <div className="container">
             <div className="navbar">
                 <div className="Logo"><NavLink to="/">
-                <img src="/photo.webp" alt="" style={{height:"100px", width: "100px", borderRadius: "50%"}} />
+                    <img src="/photo.webp" alt="" style={{ height: "100px", width: "100px", borderRadius: "50%" }} />
                 </NavLink></div>
-                <nav>
+                <nav className={showMenu ? "mob-menu" : "web-menu"}>
                     <ul>
                         <li> <NavLink to="/" >Home</NavLink></li>
                         <li> <NavLink to="events" >Events</NavLink></li>
@@ -19,7 +26,13 @@ export const Headers = () => {
                         <li> <NavLink to="contact" >Contact</NavLink></li>
                         <li> <NavLink to="login" > Login </NavLink></li>
                     </ul>
+
                 </nav>
+                <div className='ham-menu'>
+                    <button onClick={handleToggleButton} >
+                        <GiHamburgerMenu className='toggle'/>
+                    </button>
+                </div>
             </div>
         </div>
     </header>
